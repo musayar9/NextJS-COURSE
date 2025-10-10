a201e3862c87a7fca6bb1737320a1c1b939ed33d
Musa <musasayar67@gmail.com>
2025-10-10 12:29:29 +0300

    create posts component

---
+++ src/app/(marketing)/blog/Posts.tsx
@@ -0,0 +1,41 @@
+"use client"
+import { fetchPosts } from '@/db/data'
+import Link from 'next/link'
+import React, { useState } from 'react'
+
+const Posts = ({initialPosts}:{initialPosts:Awaited<ReturnType<typeof fetchPosts>>}) => {
+const [posts, setPosts] =useState(initialPosts)
+  return (
+       <ul className="space-y-4">
+        {posts.map((post) => (
+          <li
+            key={post.id}
+            className="p-4 border border-slate-100 shadow shadow-amber-50 rounded-md bg-gray-900"
+          >
+            <h2 className="text-2xl font-semibold mb-3">{post.title}</h2>
+            <p>{post.content}</p>
+            <p className="text-sm text-gray-400 mt-4">
+              By {post.authorName} on{" "}
+              {new Date(post.createdAt).toLocaleDateString()}
+            </p>
+            <div className="mt-2">
+              <span className="mr-4 text-sm font-bold">
+                Likes: {post.likeCount || 0}
+              </span>
+              <span className="text-sm font-bold">
+                Comments: {post.commentsCount || 0}
+              </span>
+            </div>
+            <Link
+              href={`/blog/${post.id}`}
+              className="text-blue-300 hover:underline mt-4 block hover:text-blue-600"
+            >
+              Read more <span className="sr-only">on {post.title}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+  )
+}
+
+export default Posts
--- src/app/(marketing)/blog/page.tsx
+++ src/app/(marketing)/blog/page.tsx
@@ -1,45 +1,25 @@
 import { fetchPosts, getPostCount } from "@/db/data";
 import Link from "next/link";
-import React from "react";
 
-const BlogPage = async () => {
+import Posts from "./Posts";
+
+const BlogPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) => {
   // const posts = await fetchPosts();
   // const count = await getPostCount();
+
+  // const page = (await searchParams).page || 1;
+
   const [posts, count] = await Promise.all([fetchPosts(), getPostCount()]);
   console.log("posts", posts);
   return (
     <div className="mt-4 mb-4 text-white">
       <h1 className="text-3xl font-bold">The Blog</h1>
       <p className="mb-4">Total Posts: {count}</p>
-      <ul className="space-y-4">
-        {posts.map((post) => (
-          <li
-            key={post.id}
-            className="p-4 border border-slate-100 shadow shadow-amber-50 rounded-md bg-gray-900"
-          >
-            <h2 className="text-2xl font-semibold mb-3">{post.title}</h2>
-            <p>{post.content}</p>
-            <p className="text-sm text-gray-400 mt-4">
-              By {post.authorName} on{" "}
-              {new Date(post.createdAt).toLocaleDateString()}
-            </p>
-            <div className="mt-2">
-              <span className="mr-4 text-sm font-bold">
-                Likes: {post.likeCount || 0}
-              </span>
-              <span className="text-sm font-bold">
-                Comments: {post.commentsCount || 0}
-              </span>
-            </div>
-            <Link
-              href={`/blog/${post.id}`}
-              className="text-blue-300 hover:underline mt-4 block hover:text-blue-600"
-            >
-              Read more <span className="sr-only">on {post.title}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <Posts  initialPosts={posts}/>
     </div>
   );
 };
